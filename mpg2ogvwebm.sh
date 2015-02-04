36a815609f754bb51855c8b7fb1b2c600c6c965a
Nando Rossi <hello@nandorocker.com>
2015-02-04 17:08:53 +0100

    Script now asks whether to overwrite files.
    Theora only; not quite there yet.
    Gotta turn the whole thing into a loop.

--- mpg2ogvwebm.sh
+++ mpg2ogvwebm.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+### Check this article out: http://askubuntu.com/questions/343727/filenames-with-spaces-breaking-for-loop-find-command
+
 # Only execute actions if there are mp4 files in directory
 if [ "$(find $1 -type f -name '*.mp4')" ]; then
 	for filename in `find $1 -type f -name '*.mp4'`; do
@@ -7,41 +9,44 @@ if [ "$(find $1 -type f -name '*.mp4')" ]; then
 		# Break up file name into variables filename & extension
 		extension="${filename##*.}"
 		filename="${filename%.*}"
-		echo "Converting file $filename.$extension...\n"
+		echo "\nConverting file $filename.$extension...\n"
 
 		# THEORA CONVERT
 		#
-		echo "Ogg Theora\n----------"
-		ffmpeg2theora "$filename.mp4"
+		echo "\nOgg Theora\n----------"
 
-		### Adding this later
-		# # Check if theora file exists
-		# if [ -e "$filename.ogv" ]
-		# then
-		# 	echo "Destination file $filename.ogv already exists. Overwrite? [y,N]"
-		# 	read -n 1 overwrite
-		# 	### Find a way to put question and answer on same line
+		# Check if theora file exists
+		if [ -e "$filename.ogv" ]
+		then
+			until [ "$overwrite" = "y" ] || [ "$overwrite" = "n" ]; do
+				echo "Destination file $filename.ogv already exists. Overwrite? [y,N]"
+				read -n 1 overwrite
 
-		# 	case $overwrite in
-		# 		y) echo "\nOVERWRITING FILE."
-		# 		;;
-		# 		n) echo "\nNOT OVERWRITING FILE."
-		# 		;;
-		# 		"") echo "\nNOT OVERWRITING FILE."
-		# 		;;
-		# 		*) echo "Please answer 'y' or 'n'"
-		# 		### Find a way to go back to top of the question
-		# 		;;
-		# 	esac
-		# else 
-		# 	echo "DESTINATION FILE DOESN'T EXIST; CONVERTING."
-		# 	#ffmpeg2theora "$filename.mp4"
-		# fi
+				case $overwrite in
+					y) #echo "\nOVERWRITING FILE."
+						ffmpeg2theora "$filename.mp4"
+						
+					;;
+					n) echo "\nNOT OVERWRITING FILE."
+					;;
+					### TODO Find way to make "return" be the default
+					# "") let overwrite="y"
+					# 	echo "\nNOT OVERWRITING FILE."
+					# ;;
+					*) echo "Invalid choice. Please type 'y' or 'n'"
+					### Find a way to go back to top of the question
+					;;
+				esac
+			done
+		else 
+			#echo "DESTINATION FILE DOESN'T EXIST; CONVERTING."
+			ffmpeg2theora "$filename.mp4"
+		fi
 
 		# WEBM CONVERT
 		#
-		echo "Webm\n----"
-		ffmpeg -i "$filename.mp4" "$filename.webm"
+		echo "\nWebm\n----"
+		# ffmpeg -i "$filename.mp4" "$filename.webm"
 	done
 else
     echo "No mp4 files found in directory '$1'."
